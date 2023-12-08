3202b8d99f7187a8746f91d791966cf932f7182d
BingFengHung <joe94008@gmail.com>
2023-12-08 13:16:09 +0800

    <feat>: 新增 custom.css 檔案 因為 pkg 打包出來後 swagger-ui.css 並沒有順利產生對應的 css 檔案，而是變成 index.html 的內容，所以另外將 swagger-ui.css 檔案命名為 custom.css，再透過 swagger-ui-express 的設定將此 css 檔案導入

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import swaggerUi from "swagger-ui-express";
 import Router from './routes';  
 
 import './utils'
+import path from 'path';
+import fs from 'fs';
 
 const PORT = process.env.PORT || 8888;
 
@@ -14,15 +16,22 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+// 在開發環境中直接使用 __dirname 
+let options = { 
+  swaggerOptions: { 
+    url: '/swagger.json' 
+  }, 
+  customCssUrl: '/custom.css' 
+};
+
 app.use(
   "/docs",
   swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
-  })
+  swaggerUi.setup(undefined, options),
 )
+
+
 app.use(Router);
 
 app.get('/', async(req, res)=> {
@@ -30,5 +39,5 @@ app.get('/', async(req, res)=> {
 })
 
 app.listen(PORT, ()=> {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}/docs`);
 })
